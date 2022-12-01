ce9b9884936279946f1d295e1a467199e5cc4b93
Santiago Gamond <ssquarkss@gmail.com>
2022-12-01 16:00:51 -0300

    first

---
+++ excercise1.js
@@ -0,0 +1,27 @@
+const gifts = ['book', 'game', 'socks']
+
+function wrapping(gifts){
+    let toReturnValue = []
+    for(let i=0; i<gifts.length; i++){
+        let gift = gifts[i]
+        let numerales = "";
+        let largo = gifts[i].length + 2;
+        for(let j=0; j<largo; j++){
+           numerales += "*"
+           
+        }
+      let arraisito = [];
+      arraisitoIndex = numerales +  "\n*" + gift + "*" + "\n" + numerales ;
+      toReturnValue.push(arraisitoIndex);
+ 
+    }
+ return toReturnValue
+  
+}
+
+wrapping(gifts)
+
+
+//arraisito[0] = numerales;
+//arraisito[1] = "\n*" + gifts[i] + "*";
+//arraisito[2] = "\n" + numerales ;
